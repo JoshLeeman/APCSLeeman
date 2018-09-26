a8819558a84d2e3caa7be00cfcf4e72ab1604e84
JoshLeeman <jleeman19@ssdsnassau.org>
2018-09-26 12:56:10 -0400

    asciiart final

---
+++ AsciiArt/src/MyAsciiArt.java
@@ -0,0 +1,17 @@
+
+public class MyAsciiArt {
+
+
+public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		System.out.println("  .-~~~~-.");
+		System.out.println(" /        \\ ");
+		System.out.println(" | /  \\  /  `~~~~~-." );
+		System.out.println(" ||    |  0          @ ");
+		System.out.println(" ||    |  _.          | ");
+		System.out.println(" \\|    |   \\         / ");
+		System.out.println("  \\    /  /`~~~~~~ ` ");
+		System.out.println("  ('~~''''`) ");
+		System.out.println("  //`'''''`\\ ");
+		}
+}
